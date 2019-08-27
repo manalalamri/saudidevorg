3269e2f0a39f1b0fd3717a5b2920283802cc42cc
manalalamri <47380434+manalalamri@users.noreply.github.com>
2019-08-27 23:24:06 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,2 @@
-function programers_names(){ //test is the identifier which identifyies the function's name 
-  programers_names = ["Manal", "Hind", "Hissa"]; //programers_names is the identifier
-document.write(programers_names);
-}
-//call the function
-programers_names();
-  
-
+var b=3; var c=5; var d=9;
+document.write(b+c+d)
