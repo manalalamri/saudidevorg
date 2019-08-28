fcda076f92a6832149f402b71a4e41b55ddf06de
manalalamri <47380434+manalalamri@users.noreply.github.com>
2019-08-29 00:38:24 +0300

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,11 @@
+
+
+
+var num1 = 2;
+var num2 = .5;
+var num3 = 4;
+var result = num1+num2;
+document.write ('Sum of num1 and num2: '+ result + ' ');
+document.write ('<br> Round of num2:' + Math.round (num2)+' ');
+document.write ('<br> Min number: '+ Math.min (num1, num2, num3)+' '); 
+document.write ('<br>Max number:' + Math.max (num1, num2, num3) +'');
